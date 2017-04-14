c08156e6ce9153d751288449b1a7d6dd333d8458
SanjeevaKengeri <sanjeev.kengeri@gmail.com>
2017-04-14 12:47:45 +0530

    Create pdfToText.py

---
+++ pdfToText.py
@@ -0,0 +1,29 @@
+import sys
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+from cStringIO import StringIO
+
+def pdfparser(data):
+
+    fp = file(data, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        data =  retstr.getvalue()
+
+    print data
+
+if __name__ == '__main__':
+    pdfparser(sys.argv[1])  
+    
+#You probably want to do retstr.seek(0) after getting data, or you'll accumulate text from all the pages.
